529dc1a21d86553d2da11dbbd89dd14aea53b522
Trần Hà Đăng Khoa <92510208+DangKhoa2003@users.noreply.github.com>
2023-04-25 16:52:35 +0700

    get API

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import productRoutes from './routes/products.js'
+import cors from "cors";
+import dotenv from "dotenv";
+import productRoutes from "./routes/products.js";
 
 const app = express();
 
 dotenv.config();
 
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/products', productRoutes);
+app.use("/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello API");
+});
 
 // const CONNECTION_URL = 'mongodb+srv://javascript:javascript123@atlascluster.ndzewb3.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => {
-        console.log(`Server running on port: http://localhost:${PORT}/products`);
-    }))
-    .catch((error) => console.log(error.message));
-
-
-
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running on port: http://localhost:${PORT}/products`);
+    })
+  )
+  .catch((error) => console.log(error.message));
